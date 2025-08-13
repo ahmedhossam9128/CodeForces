e235a72db7e6e1b2931013cf8b6c8f1f26b4b36b
ahmedhossam9128 <ahmedhossam9128Qgmail.com>
2025-08-13 19:35:53 +0300

    Task 4

---
+++ A Level/Team.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    bool Petya,Vasya,Tonya;
+    int implementations;
+    do
+        cin >> n;
+    while(n < 1 || n > 1000);
+
+    for(int i = 0; i < n; i++)
+        {
+            cin >> Petya >> Vasya >> Tonya;
+            if((Petya & Vasya ) || (Petya & Tonya) || (Vasya & Tonya))
+                implementations++;
+        }
+    cout << implementations << endl;
+    return 0;
+}
