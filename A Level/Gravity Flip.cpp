a44d88b22155f548ea3d452a6dadc321fb706dd3
ahmedhossam9128 <ahmedhossam9128Qgmail.com>
2025-08-14 20:11:43 +0300

    Up to Task 6

---
+++ A Level/Gravity Flip.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void sort(int*, int);
+void swap(int*, int*);
+
+int main()
+{
+    int n;
+    int* box = new int;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> *(box+i);
+    }
+    sort(box, n);
+    for(int i = 0; i < n; i++)
+    {
+        cout << *(box+i) << " ";
+    }
+
+}
+
+void sort(int* box,int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            if(*(box+i) > *(box+j))
+                swap(box+i,box+j);
+        }
+    }
+}
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
